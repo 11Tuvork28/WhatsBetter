1e2e37900a1beef780eb3906e6564ad1217fce0d
11tuvork28 <zofi@xamh.de>
2021-11-07 18:54:33 +0100

    Vue works now

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,22 +4,55 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue').default;
+import Vue from "vue";
+import moment from "moment";
+
+import { Form, HasError, AlertError } from "vform";
+window.Form = Form;
+
+import Gate from "./Gate";
+Vue.prototype.$gate = new Gate(window.user);
+
+import Swal from "sweetalert2";
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+});
+window.Swal = Swal;
+window.Toast = Toast;
+
+import VueProgressBar from "vue-progressbar";
+Vue.use(VueProgressBar, {
+    color: "rgb(143, 255, 199)",
+    failedColor: "red",
+    height: "3px",
+});
+
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
 
 /**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * Routes imports and assigning
  */
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+import routes from "./routes";
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const router = new VueRouter({
+    mode: "history",
+    routes,
+});
+// Routes End
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +60,29 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+// Components
+Vue.component("pagination", require("laravel-vue-pagination"));
+Vue.component("app", require("./components/App.vue"));
+// Filter Section
+
+Vue.filter("myDate", function (created) {
+    return moment(created).format("MMMM Do YYYY");
+});
+
+Vue.filter("yesno", (value) =>
+    value
+        ? '<i class="fas fa-check green"></i>'
+        : '<i class="fas fa-times red"></i>'
+);
+
+// end Filter
+
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue")
+);
+
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    router,
 });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -76,8 +75,10 @@
         </nav>
 
         <main class="py-4">
+                    <router-view></router-view>
             @yield('content')
         </main>
     </div>
+        <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
