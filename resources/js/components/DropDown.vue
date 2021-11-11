b63a762913a2a73bece603bdb2374c698309272e
11tuvork28 <zofi@xamh.de>
2021-11-11 21:16:42 +0100

    added dropdown menu

---
+++ resources/js/components/DropDown.vue
@@ -0,0 +1,44 @@
+<template>
+  <Menu as="div" class="relative inline-block text-left">
+    <MenuButton class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">More</MenuButton>
+    <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-in"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
+      >
+    <MenuItems>
+    <div class="px-1 py-1">
+      <MenuItem v-slot="{ active }">
+        <a :class='{ "bg-blue-500": active }' href="/account-settings">
+          Account settings
+        </a>
+      </MenuItem>
+    </div>
+    <div class="px-1 py-1">
+      <MenuItem v-slot="{ active }">
+        <a :class='{ "bg-blue-500": active }' href="/account-settings">
+          Documentation
+        </a>
+      </MenuItem>
+    </div>
+    </MenuItems>
+    </transition>
+  </Menu>
+</template>
+
+<script>
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+
+
+export default {
+  components: {
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem,
+  },
+}
+</script>
