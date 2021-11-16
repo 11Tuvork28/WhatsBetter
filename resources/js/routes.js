f9626b18b08f1b5ec7a9590880c9e3f55811e483
11tuvork28 <zofi@xamh.de>
2021-11-16 07:37:21 +0100

    Fixed typo

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,9 +1,10 @@
-export default [{
+export default [
+    {
         path: "/",
         component: require("./components/App.vue").default,
     },
     {
         path: "/settings",
-        component: require("./components/settings.vue").default,
+        component: require("./components/Settings.vue").default,
     },
 ];
