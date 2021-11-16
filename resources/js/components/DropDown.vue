06c7d852c646a843513026a27da0e8db9086d318
11tuvork28 <zofi@xamh.de>
2021-11-16 07:39:59 +0100

    Added dropdown menu with styles

--- resources/js/components/DropDown.vue
+++ resources/js/components/DropDown.vue
@@ -1,6 +1,6 @@
 <template>
   <Menu as="div" class="relative inline-block text-left">
-    <MenuButton class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">More</MenuButton>
+    <MenuButton class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">{{ props.username }}</MenuButton>
     <transition
         enter-active-class="transition duration-100 ease-out"
         enter-from-class="transform scale-95 opacity-0"
@@ -9,19 +9,20 @@
         leave-from-class="transform scale-100 opacity-100"
         leave-to-class="transform scale-95 opacity-0"
       >
-    <MenuItems>
+    <MenuItems class="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
     <div class="px-1 py-1">
       <MenuItem v-slot="{ active }">
-        <a :class='{ "bg-blue-500": active }' href="/account-settings">
-          Account settings
-        </a>
+        <router-link :class='{ "bg-pink-500": active }' class="group flex rounded-md items-center w-full px-2 py-2 text-sm" to="settings">
+          settings
+        </router-link>
       </MenuItem>
     </div>
     <div class="px-1 py-1">
       <MenuItem v-slot="{ active }">
-        <a :class='{ "bg-blue-500": active }' href="/account-settings">
-          Documentation
-        </a>
+        <form :class='{ "hover:bg-pink-500": active }' class="group flex rounded-md items-center w-full px-2 py-2 text-sm" action="http://localhost:8008/logout" method="POST">
+          <input type="hidden" name="_token" :value="props.csrf">
+          <button type="submit">logout</button>
+        </form>
       </MenuItem>
     </div>
     </MenuItems>
@@ -34,11 +35,18 @@ import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 
 
 export default {
+  props: ['csrf', 'username'],
   components: {
     Menu,
     MenuButton,
     MenuItems,
     MenuItem,
   },
+  setup(props){
+    console.log(props.csrf);
+    return{
+      props,
+    }
+  }
 }
 </script>
